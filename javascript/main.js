4ea160fd6f53fda2fd88300ae057503e0983156e
vitezslavcapek <vitezslav.capek@educanet.cz>
2026-05-21 16:01:44 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -25,7 +25,7 @@ controls.dampingFactor = 0.05;
 // 4. Načtení 3D modelu a jeho rozmístění
 const loader = new GLTFLoader();
 loader.load(
-  './kostka.glb', // Kostka umístěná vedle index.html
+  './zamekula.glb', // Kostka umístěná vedle index.html
   function (gltf) {
     const baseCube = gltf.scene;
     
@@ -37,15 +37,7 @@ loader.load(
     cube1.position.set(0, 0, 0); 
     scene.add(cube1);
 
-    // --- LEVÁ KOSTKA ---
-    const cube2 = baseCube.clone(); 
-    cube2.position.set(-80, 0, 0); // Výrazný posun doleva na ose X
-    scene.add(cube2);
-
-    // --- PRAVÁ KOSTKA ---
-    const cube3 = baseCube.clone(); 
-    cube3.position.set(80, 0, 0);  // Výrazný posun doprava na ose X
-    scene.add(cube3);
+ 
 
     console.log("Všechny 3 kostky byly úspěšně načteny a rozmístěny!");
   },
