602350c86a765c56d020b784872513ef66855e09
vitezslavcapek <vitezslav.capek@educanet.cz>
2026-06-04 15:05:23 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -25,7 +25,7 @@ controls.dampingFactor = 0.05;
 // 4. Načtení 3D modelu a jeho rozmístění
 const loader = new GLTFLoader();
 loader.load(
-  './zemekula.glb', // Kostka umístěná vedle index.html
+  '../zemekula.glb', // Kostka umístěná vedle index.html
   function (gltf) {
     const baseCube = gltf.scene;
     
