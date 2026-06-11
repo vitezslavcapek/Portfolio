ce87ba057348039947213bbafcf6b90e9c31457c
vitezslavcapek <vitezslav.capek@educanet.cz>
2026-06-11 14:58:14 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -7,7 +7,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // Kamera je posunutá na hodnotu 150, aby obsáhla všechny tři velké kostky najednou
-camera.position.z = 150; 
+camera.position.z = 15; 
 
 // Globální proměnné pro ovládání
 let controls;
